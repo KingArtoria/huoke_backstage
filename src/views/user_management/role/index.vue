0df5f34cd42725dbcf04a5bc30bd10b17a7a1a5a
吴闯 <wuchuang456@126.com>
2022-05-12 14:32:43 +0800

    角色管理加权限

--- src/views/user_management/role/index.vue
+++ src/views/user_management/role/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-page">
-    <header class="app-header">
+    <header v-if="addPermission" class="app-header">
       <el-button type="primary" @click="$refs.form.open()">添加角色</el-button>
     </header>
     <el-table v-loading="tableLoading" :data="tableData" :header-cell-style="_headerCellStyle" border element-loading-spinner="el-icon-loading" element-loading-text="加载中，请稍候……">
@@ -8,9 +8,9 @@
       <el-table-column label="角色名称" prop="role_name"></el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
-          <el-button type="text" @click="$refs.form.open(scope.row)"> 编辑 </el-button>
-          <el-button type="text" @click="del({ id: scope.row.id })"> 删除 </el-button>
-          <el-button type="text" @click="$router.push(`/role/bind?id=${scope.row.id}&rule=${scope.row.rule}`)"> 分配权限 </el-button>
+          <el-button v-if="editPermission" type="text" @click="$refs.form.open(scope.row)"> 编辑 </el-button>
+          <el-button v-if="delPermission" type="text" @click="del({ id: scope.row.id })"> 删除 </el-button>
+          <el-button v-if="bindPermission" type="text" @click="$router.push(`/role/bind?id=${scope.row.id}&rule=${scope.row.rule}`)"> 分配权限 </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -25,6 +25,7 @@
 import listMixin from '@/mixins/listMixin';
 import RoleForm from './role-form.vue';
 import { getRoleList, delRole } from '../../../utils/api';
+import { getPermission } from '../../../utils/index';
 export default {
   mixins: [listMixin],
   components: { RoleForm },
@@ -34,6 +35,24 @@ export default {
         del: delRole,
       },
     };
+  },
+  computed: {
+    // 【添加】权限
+    addPermission() {
+      return getPermission('用户管理', '角色管理', '添加角色')
+    },
+    // 【编辑】权限
+    editPermission() {
+      return getPermission('用户管理', '角色管理', '编辑角色')
+    },
+    // 【删除】权限
+    delPermission() {
+      return getPermission('用户管理', '角色管理', '删除角色')
+    },
+    // 【分配权限】权限
+    bindPermission() {
+      return getPermission('用户管理', '角色管理', '分配权限')
+    },
   },
   methods: {
     // 获取发布内容列表
