3f506da8d9c7ea834613c6c1053c5cf7362b2772
[AnTanZhi] <[gaojiuan@163.com]>
2022-05-12 16:36:41 +0800

    个人池

--- src/views/support/customer/per-member.vue
+++ src/views/support/customer/per-member.vue
@@ -2,21 +2,13 @@
   <!-- 个人池 -->
   <div class="app-page">
     <Search :searchParams="searchParams" @search="search" />
-    <el-table v-loading="tableLoading" :data="tableData" :header-cell-style="_headerCellStyle" border
-      element-loading-spinner="el-icon-loading" element-loading-text="加载中，请稍候……">
-      <el-table-column label="ID" prop="id" width="80" align="center"></el-table-column>
-      <el-table-column label="账号" prop="account"></el-table-column>
+    <el-table v-loading="tableLoading" :data="tableData" :header-cell-style="_headerCellStyle" border element-loading-spinner="el-icon-loading" element-loading-text="加载中，请稍候……">
+      <el-table-column label="ID" prop="id" width="60" align="center"></el-table-column>
       <el-table-column label="昵称" prop="nick_name"></el-table-column>
-      <el-table-column label="手机" prop="phone" width="80" align="center"></el-table-column>
-      <el-table-column label="是否电联" prop=""></el-table-column>
-      <el-table-column label="名片——文字" prop="" width="120" align="center"></el-table-column>
-      <el-table-column label="名片——图片" prop="" width="80" align="center"></el-table-column>
-      <el-table-column label="来源" prop="source" width="150" align="center"></el-table-column>
-      <el-table-column label="关键词" prop=""></el-table-column>
-      <el-table-column label="支持" prop=""></el-table-column>
-      <el-table-column label="注册时间" prop="add_time"></el-table-column>
-      <el-table-column label="备注" prop=""></el-table-column>
-      <el-table-column label="操作" width="300">
+      <el-table-column label="手机" prop="phone" width="110" align="center"></el-table-column>
+      <el-table-column label="来源" prop="source"></el-table-column>
+      <el-table-column label="注册时间" prop="add_time" align="center" width="140"></el-table-column>
+      <el-table-column label="操作" width="240" align="center">
         <template slot-scope="scope">
           <el-button type="text">用户详情</el-button>
           <el-button type="text">设置备注</el-button>
@@ -26,35 +18,35 @@
       </el-table-column>
     </el-table>
     <footer class="app-pagination-wrap">
-      <el-pagination :page-sizes="pageSizes" background layout="prev, pager, next, jumper" :total="total"
-        :page-size="searchParams.num" :current-page="searchParams.page" @size-change="handleSizeChange"
-        @current-change="handleCurrentChange" />
+      <el-pagination :page-sizes="pageSizes" background layout="prev, pager, next, jumper" :total="total" :page-size="searchParams.num" :current-page="searchParams.page" @size-change="handleSizeChange" @current-change="handleCurrentChange" />
     </footer>
   </div>
 </template>
 
 <script>
-import Search from "./components/search.vue";
-import { getPerMember } from "@/utils/api";
-import listMixin from "@/mixins/listMixin";
+import Search from './components/search.vue';
+import { getPerMember } from '@/utils/api';
+import listMixin from '@/mixins/listMixin';
 export default {
   mixins: [listMixin],
   components: { Search },
   data() {
     return {
       searchParams: {
-        phone: "", // 手机号
-        source: "", // 来源
-        support_level: "", // 用户评级
-        vip_end: "", // 会员到期时间
+        phone: '', // 手机号
+        source: '', // 来源
+        support_level: '', // 用户评级
+        vip_end: '', // 会员到期时间
       },
     };
   },
   methods: {
     fetchData() {
-      getPerMember(this.searchParams).then((res) => {
-        res.data.list.forEach((v) => {
-          v.head = "https://asd.bdhuoke.com/" + v.head;
+      getPerMember(this.searchParams).then(res => {
+        res.data.list.forEach(v => {
+          v.head = 'https://asd.bdhuoke.com/' + v.head;
+          // 替换v.phone中间4位为*
+          v.phone = v.phone.replace(/^(\d{3})\d{4}(\d{4})$/, '$1****$2');
         });
         this.tableData = res.data.list;
         this.total = res.data.rows;
